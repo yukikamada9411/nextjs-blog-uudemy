304ed3f7e9d06252433a94ed725abe8367036b8b
yukikamada <k_yuki522@yahoo.co.jp>
2025-04-15 13:13:18 +0900

    all done

---
+++ pages/posts/firstPost.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <div>
+      <h1>最初の投稿</h1>
+      <Link href="/">ホームへ戻る</Link>
+      <img src="" />
+    </div>
+  );
+}
